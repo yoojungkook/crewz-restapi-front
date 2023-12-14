43eb40d4403391c09093ce323d50bceae5888a75
HmDol <vtwin321@naver.com>
2023-12-14 10:10:30 +0900

    App add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import { Link, BrowserRouter } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Router from './Router';
+import Header from './Component/Header';
+import Footer from './Component/footer';
+
 function App() {
+  const [selectedButton, setSelectedButton] = useState('홈');
+
+  const setPage = (label) => {
+    setSelectedButton(label);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='App'>
+      {/* <Row>
+        <Col xs={1}></Col>
+        <Col> */}
+          <div>
+            <Header />
+          </div>
+          <BrowserRouter>
+            <Link to="/moim/home">
+              <Button
+                variant={selectedButton === '홈' ? 'danger' : 'outline-danger'}
+                className={`menubar`}
+                onClick={() => setPage('홈')}
+              >
+                홈
+              </Button>
+            </Link>
+            <Link to="/somoim/home">
+              <Button
+                variant={selectedButton === '소모임' ? 'danger' : 'outline-danger'}
+                className={`menubar`}
+                onClick={() => setPage('소모임')}
+              >
+                소모임
+              </Button>
+            </Link>
+            <Link to="/moim/albem">
+              <Button
+                variant={selectedButton === '사진첩' ? 'danger' : 'outline-danger'}
+                className={`menubar`}
+                onClick={() => setPage('사진첩')}
+              >
+                사진첩
+              </Button>
+            </Link>
+            <Link to="/temp">
+              <Button
+                variant={selectedButton === '게시판' ? 'danger' : 'outline-danger'}
+                className={`menubar`}
+                onClick={() => setPage('게시판')}
+              >
+                게시판
+              </Button>
+            </Link>
+            <Link to="/review/ReviewTest">
+              <Button
+                variant={selectedButton === '후기' ? 'danger' : 'outline-danger'}
+                className={`menubar`}
+                onClick={() => setPage('후기')}
+              >
+                후기
+              </Button>
+            </Link>
+            <br /><hr style={{width : "200%", position : "relative", right: "60%"}}/>
+            <Router />
+          </BrowserRouter>
+          
+        {/* </Col>
+        <Col xs={1}></Col>
+      </Row> */}
+      <Footer />
+    </Container>
   );
 }
 
